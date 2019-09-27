3852720a6a2d23e89a579e12ab0ff5cf5e9b7734
Jaanus Siim <jaanus@jaanussiim.com>
2019-09-27 17:46:46 +0300

    Add frameworks

---
+++ InsightAdmin/InsightCore/Supporting files/InsightCore.h
@@ -0,0 +1,19 @@
+//
+//  InsightCore.h
+//  InsightCore
+//
+//  Created by Jaanus Siim on 27/09/2019.
+//  Copyright © 2019 Coodly OU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InsightCore.
+FOUNDATION_EXPORT double InsightCoreVersionNumber;
+
+//! Project version string for InsightCore.
+FOUNDATION_EXPORT const unsigned char InsightCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InsightCore/PublicHeader.h>
+
+
---
+++ InsightAdmin/InsightUI/Supporting files/InsightUI.h
@@ -0,0 +1,19 @@
+//
+//  InsightUI.h
+//  InsightUI
+//
+//  Created by Jaanus Siim on 27/09/2019.
+//  Copyright © 2019 Coodly OU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InsightUI.
+FOUNDATION_EXPORT double InsightUIVersionNumber;
+
+//! Project version string for InsightUI.
+FOUNDATION_EXPORT const unsigned char InsightUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InsightUI/PublicHeader.h>
+
+
